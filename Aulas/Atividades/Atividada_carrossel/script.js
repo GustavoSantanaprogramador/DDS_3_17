abe08b87807130ac902cb9a6dda97002dfedd9f4
gustavodiassantanads@gmail.com <gsantana@labcetec.net>
2024-08-21 21:07:38 -0300

    carousel funcionando

--- Aulas/Atividades/Atividada_carrossel/script.js
+++ Aulas/Atividades/Atividada_carrossel/script.js
@@ -1,13 +1,15 @@
 let count = 1;
-document.getElementById("radio1").checked =true;
+document.getElementById("radio1").checked = true;
 
 setInterval(function(){
+    nextImage();
     },2000)
 
-    function nexImage(){
+    function nextImage(){
         count++;
         if(count>3){
             count = 1;
         }
-        document.getElementById("radio1").checked = true;
+
+        document.getElementById(`radio${count}`).checked = true;
     }
